60afd09edbeb89c09524381416106a6ce4b17b5e
Jamesgit22 <jschroeder97@icloud.com>
2023-05-06 08:05:18 -0500

    Added codebase to server.js and started .env file

---
+++ server.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize');
+const { strict } = require('assert');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: 'lorem is life',
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        secure: true,
+        sameSite: strict
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(sessoin(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engin', 'handlebars'); // can change this second string to hbs for propper handlebars naming. Will have to rename files though.
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening. View at http://localhost:3001')); // add heroku link once generated.
+});
