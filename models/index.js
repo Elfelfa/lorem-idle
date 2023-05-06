e99d189b66f00a7607af7a076aab5990785e7052
Jamesgit22 <jschroeder97@icloud.com>
2023-05-06 07:43:19 -0500

    Added starting associations between models in index.

--- models/index.js
+++ models/index.js
@@ -6,4 +6,17 @@ const Users = require('./User');
 
 //TODO: create relationships between tables
 
+// Sequelize should be able to create required keys to complete associations below.
+// Also not including CASCADE as we do not want to delete material from other tables. As in if a user is deleted, we would not want to remove what type of pickaxe they have from the tool table.
+
+Users.hasMany(Resource);
+
+Users.hasMany(Skill);
+
+Users.hasMany(Tool);
+
+Users.hasOne(Experience);
+
+// Consider having an association between tool and skill. Depends on where we want to put our equip lvl requirement logic.
+
 module.exports = { Experience, Skill, Tool, Resource, Users };
