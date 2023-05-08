afbaa7482583a0ef86286833d12ba1d8861c352d
Jamesgit22 <jschroeder97@icloud.com>
2023-05-08 14:48:16 -0500

    Added router for progress routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,11 +5,14 @@ const toolRoutes = require('./toolRoutes');
 const skillRoutes = require('./skillRoutes');
 const inventoryRoutes = require('./inverntoryRoutes');
 const itemRoutes = require('./itemRoutes');
+const progressRoutes = require('./progressRoutes');
 
 router.use('/user', userRoutes);
 router.use('/tool', toolRoutes);
 router.use('/skill', skillRoutes);
 router.use('/inventory', inventoryRoutes);
-router.use('item', itemRoutes);
+router.use('/item', itemRoutes);
+router.use('/progress', progressRoutes);
+
 
 module.exports = router;
---
+++ controllers/api/progressRoutes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { Progress } = require('../../models');
+const Auth = require('../../utils/auth');
+
+
+module.exports = router;
