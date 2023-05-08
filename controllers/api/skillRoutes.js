3504dd9dc81cd845876795b897de6ab518f4f5be
Jamesgit22 <jschroeder97@icloud.com>
2023-05-08 18:12:46 -0500

    Added GET endpoints to skill router

--- controllers/api/skillRoutes.js
+++ controllers/api/skillRoutes.js
@@ -3,4 +3,25 @@ const { Skill } = require('../../models');
 const Auth = require('../../utils/auth');
 
 
+// Endpoint to GET all skills
+router.get('/', Auth, async (req, res) => {
+    try {
+        const skillData = await Skill.findAll();
+        const skillDataObj = skillData.map(data => data.get({ plain: true }));
+        res.status(200).json(skillDataObj);
+    } catch (err) {
+        res.status(400).json({ message: 'Error in getting all skills' + err});
+    }
+});
+
+// Endpoint to GET a skill by id
+router.get('/:id', Auth, async (req, res) => {
+    try {
+        const skillData = await Skill.findByPk(req.params.id);
+        res.status(200).json({skillData})
+    } catch (err) {
+        res.status(400).json({ message: 'Unable to get skill by id' + err});
+    }
+})
+
 module.exports = router;
