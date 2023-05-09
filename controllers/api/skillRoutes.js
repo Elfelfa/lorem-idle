d5f3cfb772a3681f97693d07ea4eaffd57f215db
Jamesgit22 <jschroeder97@icloud.com>
2023-05-08 22:51:27 -0500

    Reformatted code and corrected errors in user, tools, and skill routes.

--- controllers/api/skillRoutes.js
+++ controllers/api/skillRoutes.js
@@ -1,27 +1,34 @@
-const router = require('express').Router();
-const { Skill } = require('../../models');
-const Auth = require('../../utils/auth');
-
+const router = require("express").Router();
+const { Skill } = require("../../models");
+const Auth = require("../../utils/auth");
 
 // Endpoint to GET all skills
-router.get('/', Auth, async (req, res) => {
-    try {
-        const skillData = await Skill.findAll();
-        const skillDataObj = skillData.map(data => data.get({ plain: true }));
-        res.status(200).json(skillDataObj);
-    } catch (err) {
-        res.status(400).json({ message: 'Error in getting all skills' + err});
-    }
+router.get("/", Auth, async (req, res) => {
+  try {
+    const skillData = await Skill.findAll();
+    const skillDataObj = skillData.map((data) => data.get({ plain: true }));
+    res.status(200).json(skillDataObj);
+  } catch (err) {
+    res
+      .status(400)
+      .json({
+        message: "Error in getting all skills from the database. Error: " + err,
+      });
+  }
 });
 
 // Endpoint to GET a skill by id
-router.get('/:id', Auth, async (req, res) => {
-    try {
-        const skillData = await Skill.findByPk(req.params.id);
-        res.status(200).json({skillData})
-    } catch (err) {
-        res.status(400).json({ message: 'Unable to get skill by id' + err});
-    }
-})
+router.get("/:id", Auth, async (req, res) => {
+  try {
+    const skillData = await Skill.findByPk(req.params.id);
+    res.status(200).json({ skillData });
+  } catch (err) {
+    res
+      .status(400)
+      .json({
+        message: "Unable to get skill by id from the database. Error: " + err,
+      });
+  }
+});
 
 module.exports = router;
