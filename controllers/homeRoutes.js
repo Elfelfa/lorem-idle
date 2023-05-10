69c060ccb0ce56513df1c39882f590ae6acc28d7
SurosRegime <ephrain926@hotmail.com>
2023-05-10 14:40:07 -0400

    added splash.handlebars +routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,10 +7,35 @@ router.get('/', function (req, res) {
   res.render('home', { title: 'Lorem Idle', showLogin: false });
 })
 
-// Login page route.
-router.get('/login', function (req, res) {
-  res.render('home', { title: 'Login - Lorem Idle', showLogin: true });
+router.get('/login', async (req, res) => { 
+  try{
+  res.status(200).render("login");
+  } catch (err) {
+    res.status(500).json({ message: "Unable to load login page from server. Error: " + err});
+  }
+});
+
+router.get('/home', async (req, res) => {  //Add Auth helper after development.
+  try{
+  res.status(200).render("home");
+  } catch (err) {
+    res.status(500).json({ message: "Unable to load home page from server. Error: " + err});
+  }
+});
+
+router.get('/splash', function (req, res) { //splash page route
+  res.render('splash', { username: req.session.username });
 })
+
+router.get('/backpack', async (req, res) => { //Add Auth helper after development.
+  try{
+  res.status(200).render("backpack");
+  } catch (err) {
+    res.status(500).json({ message: "Unable to load backpack from server. Error: " + err});
+  }
+});
+
+
 
 module.exports = router;
 
