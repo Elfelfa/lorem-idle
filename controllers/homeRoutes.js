ce1fc73e5577e90132588a03668e8088824b9e55
Jamesgit22 <jschroeder97@icloud.com>
2023-05-10 12:18:39 -0500

    Added endpoint for backpack handlebar page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,16 +3,6 @@ const express = require('express');
 const router = express.Router();
 const Auth = require('../utils/auth')
 
-// // Home page route.
-// router.get('/', function (req, res) {
-//   res.render('home', { title: 'Lorem Idle', showLogin: false });
-// })
-
-// // Login page route.
-// router.get('/login', function (req, res) {
-//   res.render('home', { title: 'Login - Lorem Idle', showLogin: true });
-// })
-
 router.get('/', async (req, res) => {
   try {
   res.status(200).render("splash");
@@ -34,6 +24,14 @@ router.get('/home', async (req, res) => {  //Add Auth helper after development.
   res.status(200).render("home");
   } catch (err) {
     res.status(500).json({ message: "Unable to load home page from server. Error: " + err});
+  }
+});
+
+router.get('/backpack', async (req, res) => { //Add Auth helper after development.
+  try{
+  res.status(200).render("backpack");
+  } catch (err) {
+    res.status(500).json({ message: "Unable to load backpack from server. Error: " + err});
   }
 });
 
