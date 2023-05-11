a8152f067718246d920cb554d39a8226883e035f
Elfelfa <koltbodzo@gmail.com>
2023-05-11 00:33:23 -0400

    Added data functionality to profile route

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,3 @@
-
 const docBody = document.body;
 
 const profileBtn = async (e) => {
@@ -9,6 +8,13 @@ const profileBtn = async (e) => {
     });
 
     if (response.ok) {
+        const checkNodes = await document.getElementById("oldNode");
+
+        if(checkNodes)
+        {
+            checkNodes.parentNode.removeChild(checkNodes);
+        }
+
         const data = await response.json();
         const rawHTML = data.html;
         const myThing = document.createElement('div');
@@ -28,49 +34,97 @@ const profileBtn = async (e) => {
 
 const backpackBtn = async (e) => {
     e.preventDefault();
-    let tab_id = e.target.getAttribute("data_id");
-    console.log(JSON.stringify({ tab_id }));
-    const response = await fetch(`/home/${tab_id}`, {
+    const response = await fetch(`/home/backpack`, {
         method: "GET",
         headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
-        document.location.replace("/home");
+        const checkNodes = await document.getElementById("oldNode");
+
+        if(checkNodes)
+        {
+            checkNodes.parentNode.removeChild(checkNodes);
+        }
+
+        const data = await response.json();
+        const rawHTML = data.html;
+        const myThing = document.createElement('div');
+        myThing.innerHTML = rawHTML;
+        docBody.appendChild(myThing);
+
+        while (myThing.firstChild) {
+            myThing.parentNode.insertBefore(myThing.firstChild,
+                myThing);
+        }
+
+        myThing.parentNode.removeChild(myThing);
     } else {
-        alert("Unable to load backpack");
+        alert("Unable to load profile");
     }
 };
 
 const woodcuttingBtn = async (e) => {
     e.preventDefault();
-    let id = e.target.getAttribute("data_id");
-    const response = await fetch("/home", {
-        method: "POST",
-        body: JSON.stringify({ id: id }),
+    const response = await fetch("/home/woodcutting", {
+        method: "GET",
         headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
-        document.location.replace("/home");
+        const checkNodes = await document.getElementById("oldNode");
+
+        if(checkNodes)
+        {
+            checkNodes.parentNode.removeChild(checkNodes);
+        }
+
+        const data = await response.json();
+        const rawHTML = data.html;
+        const myThing = document.createElement('div');
+        myThing.innerHTML = rawHTML;
+        docBody.appendChild(myThing);
+
+        while (myThing.firstChild) {
+            myThing.parentNode.insertBefore(myThing.firstChild,
+                myThing);
+        }
+
+        myThing.parentNode.removeChild(myThing);
     } else {
-        alert("Unable to load woodcutting");
+        alert("Unable to load profile");
     }
 };
 
 const fishingBtn = async (e) => {
     e.preventDefault();
-    let id = e.target.getAttribute("data_id");
-    const response = await fetch("/home", {
-        method: "POST",
-        body: JSON.stringify({ id: id }),
+    const response = await fetch("/home/fishing", {
+        method: "GET",
         headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
-        document.location.replace("/home");
+        const checkNodes = await document.getElementById("oldNode");
+
+        if(checkNodes)
+        {
+            checkNodes.parentNode.removeChild(checkNodes);
+        }
+        
+        const data = await response.json();
+        const rawHTML = data.html;
+        const myThing = document.createElement('div');
+        myThing.innerHTML = rawHTML;
+        docBody.appendChild(myThing);
+
+        while (myThing.firstChild) {
+            myThing.parentNode.insertBefore(myThing.firstChild,
+                myThing);
+        }
+
+        myThing.parentNode.removeChild(myThing);
     } else {
-        alert("Unable to load fishing");
+        alert("Unable to load profile");
     }
 };
 
